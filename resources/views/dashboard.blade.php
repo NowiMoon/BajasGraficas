4c2fac5ae66a821c10a8c5b13f9c8f82a9d60238
Luis-MirandaR <111241916+Luis-MirandaR@users.noreply.github.com>
2025-05-19 18:11:15 -0600

    Recarga de pagina al subir los datos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -720,6 +720,7 @@ $(document).ready(function() {
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             `);
+            location.reload();
         }
     });
 });
